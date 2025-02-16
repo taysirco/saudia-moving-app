5467cf62768c19b58e555841cd26e87d4e234a18
Ahmed Salem <taysirco@gmail.com>
2025-02-16 03:29:19 +0200

    update

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -1,40 +1,91 @@
-import { writeFileSync } from 'fs'
-import { cities, keywords } from '../lib/utils/data'
+const { writeFileSync } = require('fs')
+const globby = require('globby')
+const prettier = require('prettier')
 
-function generateSitemap() {
-  const baseUrl = 'https://saudiamoving.com'
-  let sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n'
-  sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+async function generate() {
+  const prettierConfig = await prettier.resolveConfig('./.prettierrc')
+  
+  // قائمة الصفحات الثابتة
+  const pages = [
+    '',
+    'about',
+    'contact',
+    'services',
+    'cities',
+    'blog',
+    'terms',
+    'privacy',
+  ]
 
-  // Add homepage
-  sitemap += `  <url>
-    <loc>${baseUrl}</loc>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>\n`
+  // قائمة المدن
+  const cities = [
+    'riyadh',
+    'jeddah', 
+    'dammam',
+    'mecca',
+    'medina',
+    'tabuk',
+    'abha',
+    'khamis-mushait',
+    'taif',
+    'yanbu',
+    'jubail',
+    'khobar',
+  ]
 
-  // Add city pages
-  cities.forEach(city => {
-    sitemap += `  <url>
-    <loc>${baseUrl}/${encodeURIComponent(city)}</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>\n`
+  // قائمة الخدمات
+  const services = [
+    'furniture-moving',
+    'office-moving',
+    'villa-moving',
+    'apartment-moving',
+    'storage',
+    'packing',
+  ]
 
-    // Add service pages for each city
-    keywords.forEach(service => {
-      sitemap += `  <url>
-    <loc>${baseUrl}/${encodeURIComponent(city)}/${encodeURIComponent(service)}</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.6</priority>
-  </url>\n`
-    })
+  const routes = [
+    // الصفحات الثابتة
+    ...pages.map((page) => ({
+      url: `https://saudiamoving.com/${page}`,
+      lastModified: new Date().toISOString(),
+    })),
+
+    // صفحات المدن
+    ...cities.map((city) => ({
+      url: `https://saudiamoving.com/cities/${city}`,
+      lastModified: new Date().toISOString(),
+    })),
+
+    // صفحات الخدمات
+    ...services.map((service) => ({
+      url: `https://saudiamoving.com/services/${service}`,
+      lastModified: new Date().toISOString(),
+    })),
+  ]
+
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${routes
+        .map((route) => {
+          return `
+            <url>
+              <loc>${route.url}</loc>
+              <lastmod>${route.lastModified}</lastmod>
+            </url>
+          `
+        })
+        .join('')}
+    </urlset>
+  `
+
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: 'html',
   })
 
-  sitemap += '</urlset>'
-
-  writeFileSync('public/sitemap.xml', sitemap)
+  writeFileSync('public/sitemap.xml', formatted)
   console.log('Sitemap generated successfully!')
 }
 
-generateSitemap() 
+generate() 
